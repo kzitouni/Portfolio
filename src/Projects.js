1c987b719b16671c01735917e6c06284655713b9
kzitouni <zkarim091019@gmail.com>
2020-01-26 22:11:41 -0500

    fixes

--- src/Projects.js
+++ src/Projects.js
@@ -82,9 +82,9 @@ const Projects = () => {
             <div className="TextBox">
               <p>
                 A production ready React Native app integrated with Stripe and
-                paypal where users could buy and sell closed college classes
-                from one another AWS was utilized for user authentication, No
-                SQL database, and web scraping
+                paypal where users could buy and sell closed college classes.
+                AWS was utilized for user authentication, No
+                SQL database, and web scraping.
               </p>
             </div>
             <div className="ProjectBottom">
